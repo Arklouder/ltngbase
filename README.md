c49d02d84b0a82f4634ab164ea293d1806d451c3
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2016-10-11 18:08:08 -0500

    sfd error handling

---
+++ README.md
@@ -0,0 +1,25 @@
+# ltngbase
+
+The goal of this project is make lightning component development easier.  I hope all of these are eventually replaced by standard components/events and I can retire all this :)
+
+
+## Streamer
+component that you set a topic for.  It connects to the streaming api and subscribes to that topic.  
+
+## SingleFieldDisplayer
+Pass in any record, and the corresponding describe for that object (use the describe method in Ora).
+
+This component will handle all the field types and properly display itself.  If the field is editable, it'll have two options--either emit a **fieldChangeEvent** that can be handled upstream, OR directly update the record in Salesforce.
+
+Errors are handled by the LightningErrorHandler component--install that in your outermost component that's using SingleFieldDisplayer
+
+## SingleFieldDisplayerReadOnly
+Faster, simpler, more performant version of SFD for when there isn't any update that'll happen.
+
+## LightningErrorHandler
+
+displays errors from apex callbacks.  Simply pass in the errors object from `.getErrors` to a handleCallbackError event and it'll handle the rest.  You can optionally name the ErrorHandler and put that name in the handleCallbackError event 
+
+## LightningDataTable
+
+pass in an array of records (presumably from a parent component) and which fields you want shown/editable.  It'll get the necessary describe info to properly label the columns and then implements SFD/SFDRO for the cells as appropriate. 
--- src/aura/SingleFieldDisplayer/SingleFieldDisplayerHelper.js
+++ src/aura/SingleFieldDisplayer/SingleFieldDisplayerHelper.js
@@ -27,36 +27,13 @@
                 if (state === "SUCCESS") {
                     console.log(a);
                     component.set("v.simpleOutput", fieldValue);
-                }  else if (state === "ERROR") {
-                    
-                    var errors = a.getError();
-                    console.log(errors)
-                    if (errors) {
-                        errors.forEach( function (error){
-
-                            //top-level error.  there can be only one
-                            if (error.message){
-                                helper.toastThis(error.message);                    
-                            }
-
-                            //page-level errors (validation rules, etc)
-                            if (error.pageErrors){
-                                error.pageErrors.forEach( function(pageError) {
-                                    helper.toastThis(pageError.message)                     
-                                });                 
-                            }
-
-                            if (error.fieldErrors){
-                                //field specific errors--we'll say what the field is                    
-                                for (var fieldName in error.fieldErrors) {
-                                    //each field could have multiple errors
-                                    error.fieldErrors[fieldName].forEach( function (errorList){ 
-                                        helper.toastThis(errorList.message, "Field Error on " + fieldName + " : ")                          
-                                    });                                
-                                };  //end of field errors forLoop                   
-                            } //end of fieldErrors if
-                        }); //end Errors forEach
-                    }	
+                }  else if (state === "ERROR") {                    
+                    var appEvent = $A.get("e.c:handleCallbackError");
+                    appEvent.setParams({
+                        "errors" : a.getError(),
+                        "errorComponentName" : "anythingPath"
+                    });
+                    appEvent.fire();    
                 } //end if errors            
             });
             $A.enqueueAction(action);
